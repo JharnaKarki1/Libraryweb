0091f8e49ec83e51df438c561de46b7fd620e0e4
JharnaKarki1 <jharna.karki@gmail.com>
2022-07-08 21:39:38 -0500

    add publisher form completed

--- src/main/java/edu/mum/cs/cs425/elibrary/controller/PublisherController.java
+++ src/main/java/edu/mum/cs/cs425/elibrary/controller/PublisherController.java
@@ -2,10 +2,13 @@ package edu.mum.cs.cs425.elibrary.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.mum.cs.cs425.elibrary.model.Publisher;
 import edu.mum.cs.cs425.elibrary.service.PublisherService;
 @Controller
 @RequestMapping(value ={"/publisher"})
@@ -21,5 +24,24 @@ public class PublisherController {
 	        modelAndView.setViewName("publisher/list");
 	        return modelAndView;
 	    }
+//	 @GetMapping(value = {"/new"})
+//	    public String displayNewPublisherForm() {
+//		 
+//	        return "publisher/new";
+//	 }
+	 
+	 @GetMapping(value = {"/new"})
+	    public String displayNewPublisherForm(Model model) {
+	        var newPublisher = new Publisher();
+	        model.addAttribute("publisher", newPublisher);
+	        return "publisher/new";
+	    }
+	 @PostMapping(value = {"/new"}) // PRG: Post-Redirect-Get
+	    public String addNewPublisher(Publisher publisher) {
+	       
+	           
+	            return "redirect:/publisher/list";
+	 }
+	    
 
 }
--- src/main/java/edu/mum/cs/cs425/elibrary/model/PrimaryAddress.java
+++ src/main/java/edu/mum/cs/cs425/elibrary/model/PrimaryAddress.java
@@ -22,13 +22,67 @@ public class PrimaryAddress {
     private String zipCode;
 
     @OneToOne(mappedBy = "primaryAddress")
-    @JsonIgnore
+  
     private Publisher publisher;
 
-
+//    public PrimaryAddress(Integer addressId, String street, String city, String state, String zipCode) {
+//        this.addressId = addressId;
+//        this.street = street;
+//        this.city = city;
+//        this.state = state;
+//        this.zipCode = zipCode;
+//    }
+//
+//    public PrimaryAddress() {
+//    }
+//
+//    public Integer getAddressId() {
+//        return addressId;
+//    }
+//
+//    public void setAddressId(Integer addressId) {
+//        this.addressId = addressId;
+//    }
+//
+//    public String getStreet() {
+//        return street;
+//    }
+//
+//    public void setStreet(String street) {
+//        this.street = street;
+//    }
+//
+//    public String getCity() {
+//        return city;
+//    }
+//
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+//
+//    public String getState() {
+//        return state;
+//    }
+//
+//    public void setState(String state) {
+//        this.state = state;
+//    }
+//
+//    public String getZipCode() {
+//        return zipCode;
+//    }
+//
+//    public void setZipCode(String zipCode) {
+//        this.zipCode = zipCode;
+//    }
+//    @Override
+//    public String toString() {
+//        return String.format("{AddressId: %d, Street: %s, City: %s, State: %s, ZipCode: %s}"
+//                , addressId, street, city, state, zipCode);
+//    }
     @Override
     public String toString() {
-        return String.format("%s<br>%s %s %s"
+        return String.format("%s %s %s %s"
                 , street, city, state, zipCode);
     }
 }
